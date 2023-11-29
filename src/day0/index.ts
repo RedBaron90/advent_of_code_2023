9234a2e252a48a96426b746218330bca8276acfa
qxw6752 <alexander.ad.dorn@partner.bmwgroup.com>
2023-11-29 20:28:58 +0100

    day1 part1

--- src/day0/index.ts
+++ src/day0/index.ts
@@ -5,8 +5,23 @@ class Day0 extends Day {
     super(0);
   }
 
-  solveForPartOne(input: string): string {
-    return input;
+  solveForPartOne(input: string): any {
+    let array = input.split("\n");
+    let counter = 0;
+    let max = 0;
+    array.forEach((element) => {
+      if (element.length === 0) {
+        if (counter > max) {
+          max = counter;
+        }
+
+        counter = 0;
+      } else {
+        counter += Number(element);
+      }
+    });
+
+    return max;
   }
 
   solveForPartTwo(input: string): string {
